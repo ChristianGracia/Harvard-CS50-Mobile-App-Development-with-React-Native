d84453ab4cffb44046ffa4755ac72b9bc449752b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-06-29 21:26:13 +0000

    working around with creating a form for inputting to do item name with only DOM manipulation

--- Project-0/To-Do App/script.js
+++ Project-0/To-Do App/script.js
@@ -9,6 +9,19 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-function newTodo() {
-  alert('New TODO button clicked!')
+window.onload = formButton()
+
+function formButton() {
+
+  // var formAdd = document.createElement("input")
+  // var submitButton = document.createElement("input")
+  // formAdd.setAttribute("type", "text")
+  // formAdd.setAttribute("name", "todo item")
+  // submitButton.setAttribute("type", "submit")
+  // formAdd.appendChild(submitButton)
+  // var grabElement = document.getElementsByClassNames('flow-right')
+  // grabElement.parentNode.appendChild(formAdd)
+  var button1 = document.getElementsByTagName('button')
+  button1.setAttribute('type', 'submit')
+
 }
