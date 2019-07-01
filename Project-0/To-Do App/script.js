5e3d938c86decd94572a1c786237a736cb70b286
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-01 03:50:57 +0000

    format

--- Project-0/To-Do App/script.js
+++ Project-0/To-Do App/script.js
@@ -18,104 +18,104 @@ document.getElementsByTagName("H1")[0].innerHTML = "The World's Friendliest To-D
 function newTodo(item) {
 
 
-//create div
-const divContainer = document.createElement('div');
-divContainer.setAttribute("class", "ghostDiv");
+  //create div
+  const divContainer = document.createElement('div');
+  divContainer.setAttribute("class", "ghostDiv");
 
 
-//add input button
-const buttonInput = document.createElement('input');
-buttonInput.setAttribute("type", "text");
-buttonInput.setAttribute("id", "id" + itemCount);
-buttonInput.setAttribute("placeholder", "to-do item");
+  //add input button
+  const buttonInput = document.createElement('input');
+  buttonInput.setAttribute("type", "text");
+  buttonInput.setAttribute("id", "id" + itemCount);
+  buttonInput.setAttribute("placeholder", "to-do item");
 
-//add submit button
-const submitButton = document.createElement('input');
-submitButton.setAttribute("type", "submit");
-submitButton.setAttribute("onclick", "submitItem()");
-submitButton.setAttribute("name", itemCount);
+  //add submit button
+  const submitButton = document.createElement('input');
+  submitButton.setAttribute("type", "submit");
+  submitButton.setAttribute("onclick", "submitItem()");
+  submitButton.setAttribute("name", itemCount);
 
-//add buttons to html
-listItems = document.getElementById("todo-list");
+  //add buttons to html
+  listItems = document.getElementById("todo-list");
 
-//add buttons to container
-divContainer.appendChild(buttonInput);
-divContainer.appendChild(submitButton);
-// divContainer.appendChild(checkBoxes);
+  //add buttons to container
+  divContainer.appendChild(buttonInput);
+  divContainer.appendChild(submitButton);
+  // divContainer.appendChild(checkBoxes);
 
-//add buttons to html
-listItems = document.getElementById("todo-list");
-listItems.appendChild(divContainer)
+  //add buttons to html
+  listItems = document.getElementById("todo-list");
+  listItems.appendChild(divContainer)
 
-//update todo list item count
-itemCount++;
-console.log("item count: " + itemCount);
-document.getElementById('item-count').textContent = itemCount;
+  //update todo list item count
+  itemCount++;
+  console.log("item count: " + itemCount);
+  document.getElementById('item-count').textContent = itemCount;
 
-//update todo list unchecked item count
-uncheckedCount++;
-console.log("unchecked: " + uncheckedCount);
-document.getElementById('unchecked-count').textContent = uncheckedCount;
+  //update todo list unchecked item count
+  uncheckedCount++;
+  console.log("unchecked: " + uncheckedCount);
+  document.getElementById('unchecked-count').textContent = uncheckedCount;
 }
 
 function submitItem(e) {
 
 
-//click event set up
-var targ;
-if (!e) var e = window.event;
-if (e.target) targ = e.target;
-else if (e.srcElement) targ = e.srcElement;
+  //click event set up
+  var targ;
+  if (!e) var e = window.event;
+  if (e.target) targ = e.target;
+  else if (e.srcElement) targ = e.srcElement;
 
 
 
-var idCompare = "id" + e.target.name;
-var item = document.getElementById(idCompare).value;
+  var idCompare = "id" + e.target.name;
+  var item = document.getElementById(idCompare).value;
 
-if(item != ""){
+  if (item != "") {
 
-  //create li element
-  newItem = document.createElement("li");
-  newItem.setAttribute("class", "todo-text");
-  newItem.setAttribute("id", "li"+ deleteCounter);
+    //create li element
+    newItem = document.createElement("li");
+    newItem.setAttribute("class", "todo-text");
+    newItem.setAttribute("id", "li" + deleteCounter);
 
-  //add list item to li element
-  newItem.innerText = item;
-  listItems.appendChild(newItem);
+    //add list item to li element
+    newItem.innerText = item;
+    listItems.appendChild(newItem);
 
-  console.log(item);
+    console.log(item);
 
-  //add checkbox button
-  const checkBoxes = document.createElement('input');
-  checkBoxes.setAttribute("type", "checkbox");
-  checkBoxes.setAttribute("class", "todo-checkbox check");
-  checkBoxes.setAttribute("onclick", "checkBoxFunction()");
-  checkBoxes.setAttribute("id", "check" + deleteCounter);
-  checkBoxes.setAttribute("value", "1");
-  listItems.appendChild(checkBoxes)
-  buttonCounter++;
+    //add checkbox button
+    const checkBoxes = document.createElement('input');
+    checkBoxes.setAttribute("type", "checkbox");
+    checkBoxes.setAttribute("class", "todo-checkbox check");
+    checkBoxes.setAttribute("onclick", "checkBoxFunction()");
+    checkBoxes.setAttribute("id", "check" + deleteCounter);
+    checkBoxes.setAttribute("value", "1");
+    listItems.appendChild(checkBoxes)
+    buttonCounter++;
 
-  document.getElementById(idCompare).style.display = "none"
-  e.target.style.display = "none"
-
-
-
-
-  //add delete button
-  const deleteButton = document.createElement("button");
-  deleteButton.setAttribute("onclick", "deleteButton()")
-  deleteButton.setAttribute("class", "delete-button")
-  deleteButton.setAttribute("type", "button")
-  deleteButton.setAttribute("value", "button")
-  deleteButton.innerText = "Delete"
+    document.getElementById(idCompare).style.display = "none"
+    e.target.style.display = "none"
 
 
 
-  deleteButton.setAttribute("id", "delete-button" + deleteCounter)
-  deleteCounter++
+
+    //add delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("onclick", "deleteButton()")
+    deleteButton.setAttribute("class", "delete-button")
+    deleteButton.setAttribute("type", "button")
+    deleteButton.setAttribute("value", "button")
+    deleteButton.innerText = "Delete"
 
 
-  listItems.appendChild(deleteButton)
+
+    deleteButton.setAttribute("id", "delete-button" + deleteCounter)
+    deleteCounter++
+
+
+    listItems.appendChild(deleteButton)
   }
   else alert("You didn't Enter Anything!")
 }
@@ -129,21 +129,19 @@ function checkBoxFunction(e) {
   else if (e.srcElement) targ = e.srcElement;
 
   //change unchecked to-do item count when button is clicked
-  if (e.target.value == 1)
-  {
+  if (e.target.value == 1) {
     uncheckedCount--;
     document.getElementById('unchecked-count').textContent = uncheckedCount;
     e.target.value = 0;
   }
-  else
-  {
+  else {
     uncheckedCount++;
     document.getElementById('unchecked-count').textContent = uncheckedCount;
     e.target.value = 1;
   }
 }
 
-function deleteButton(e){
+function deleteButton(e) {
 
   //click event set up
   var targ;
@@ -155,14 +153,13 @@ function deleteButton(e){
 
   document.getElementById(e.target.id).style.display = "none"
 
-  if (document.getElementById("li" + idNumber[13]))
-  {
-   document.getElementById("li" + idNumber[13]).style.display = "none"
+  if (document.getElementById("li" + idNumber[13])) {
+    document.getElementById("li" + idNumber[13]).style.display = "none"
   }
-   document.getElementById("check" + idNumber[13]).style.display = "none"
+  document.getElementById("check" + idNumber[13]).style.display = "none"
 
-   itemCount--
+  itemCount--
 
-   document.getElementById('item-count').textContent = itemCount;
-   document.getElementById('unchecked-count').textContent = uncheckedCount;
+  document.getElementById('item-count').textContent = itemCount;
+  document.getElementById('unchecked-count').textContent = uncheckedCount;
 }
