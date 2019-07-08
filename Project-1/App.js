761d69bd0a0c6997d92ba82d8ba8cb862676752e
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-08 00:56:19 +0000

    added vibrate import

--- Project-1/App.js
+++ Project-1/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {vibrate} from './utils'
 
 export default class App extends React.Component {
   render() {
