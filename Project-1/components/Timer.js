2fa0b8804496f1282aa0bca15ed786fa0f8b6318
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-12 00:38:20 -0400

    more style refactoring and use of ternary operators

--- Project-1/components/Timer.js
+++ Project-1/components/Timer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+ct from 'react';
 import { StyleSheet, Text, Button, View } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -35,10 +35,11 @@ class Timer extends React.Component {
       {this.state.buttonShow ? <Button title="5 Minutes" color="#BA0ED4" onPress={this.hideTimeButtons}/>: null}
       </View>
       <View className="buttons"style={styles.button2}>
+
       {this.state.buttonShow ? <Button title= "25 minutes" color="#BA0ED4" onPress={this.hideTimeButtons}/>: null}
       </View>
 
-      {this.state.timerShow ? <Text>Hi</Text>: null}
+      {this.state.timerShow ? <Text style={styles.timerHeader}>TIMER</Text>: null}
       </View>
   )
   }
@@ -52,20 +53,22 @@ const styles = StyleSheet.create({
     fontSize: 40,
   },
   button1: {
-    borderRadius:10,
-    borderWidth: 1,
     borderColor: '#fff',
     marginRight: 30,
     backgroundColor:'#fff',
 
   },
   button2: {
-    borderRadius:10,
-    borderWidth: 1,
     borderColor: '#fff',
     marginLeft: 30,
-    backgroundColor:'#fff'
+    backgroundColor:'#fff',
   },
+  timerHeader: {
+    marginLeft: -50,
+    fontWeight: 'bold',
+    fontSize: 40,
+    color: 'red',
+  }
 
 });
 
