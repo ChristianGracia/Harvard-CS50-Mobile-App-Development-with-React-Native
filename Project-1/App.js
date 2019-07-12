0da96111a4b60fb34b919cf29ea4b3ea694227e6
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-11 23:30:54 -0400

    added toggle start buttons

--- Project-1/App.js
+++ Project-1/App.js
@@ -3,15 +3,26 @@ import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import Timer from './components/Timer.js'
 
 export default class App extends React.Component {
-  state = {
-    showButton: 'visible',
-  };
+  constructor(props) {
+    super(props)
 
-    hideButton = () => {
-      this.setState({
-        showButton: 'hidden'
-      })
-    };
+    this.state ={
+      startApp: true,
+    }
+  }
+
+hideButton = () =>{
+
+  if(this.state.startApp == true)
+  {
+    this.setState({startApp: false})
+  }
+  else
+  {
+    this.setState({startApp: true})
+  }
+}
+
 
 
 render() {
@@ -20,8 +31,8 @@ render() {
         <Text style={{fontWeight: 'bold', fontSize: 50, marginTop: 30}}>StudyCube</Text>
         <Image source={require('./assets/icon.png')} style={{width: '40%', height: '30%', marginTop: 40}} />
         <Text style={{marginTop: 30, fontSize: 22, marginBottom: 20, fontWeight: 'bold'}}>Your little productivity buddy</Text>
-        <Button title="Start Timer" style = {{visibility: this.state.showButton}} onPress = {this.hideButton}/>
-        <Timer />
+        {this.state.startApp ? <Button className= "enterButton" title="Start Timer" onPress={this.hideButton} />: null}
+        {!this.state.startApp ? <Timer />: null}
     </View>
   );
  }
