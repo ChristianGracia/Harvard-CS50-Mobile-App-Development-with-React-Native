670cc739bfcf971740d9b22cf87e2ec64f45430b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-11 23:54:32 -0400

    added 5 and 25 min buttons

--- Project-1/components/Timer.js
+++ Project-1/components/Timer.js
@@ -6,9 +6,41 @@ import PropTypes from 'prop-types';
 class Timer extends React.Component {
   render() {
     return(
-    <Button title="hi" />
+
+      <View style={styles.container}>
+      <View className="buttons" style={styles.button1}>
+      <Button title="5 Minutes" />
+      </View>
+      <View className="buttons"  style={styles.button2}>
+      <Button title= "25 minutes"/>
+      </View>
+      </View>
   )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  button1: {
+    borderRadius:10,
+    borderWidth: 2,
+    borderColor: '#fff',
+    marginRight: 30,
+
+  },
+  button2: {
+    borderRadius:10,
+    borderWidth: 2,
+    borderColor: '#fff',
+    marginLeft: 30,
+  },
+
+});
+
 
 export default Timer;
