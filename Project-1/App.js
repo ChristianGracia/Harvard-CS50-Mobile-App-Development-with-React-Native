e4346f01287cdd982a96fbed4e7c3de36d10c4fe
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-12 02:42:08 -0400

    formatted App.js, injected Clock.js into App.js

--- Project-1/App.js
+++ Project-1/App.js
@@ -1,61 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import Timer from './components/Timer.js'
+import React from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import Timer from "./components/Timer.js";
+import Clock from "./components/Clock.js";
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state ={
-      startApp: true,
+    this.state = {
+      startApp: true
     }
   }
 
-hideButton = () =>{
+  hideButton = () => {
+    if (this.state.startApp == true) {
+      this.setState({ startApp: false });
+    } else {
+      this.setState({ startApp: true });
+    }
+  };
 
-  if(this.state.startApp == true)
-  {
-    this.setState({startApp: false})
-  }
-  else
-  {
-    this.setState({startApp: true})
-  }
-}
-
-
-
-render() {
-  return (
-    <View style={styles.container}>
+  render() {
+    return (
+      <View style={styles.container}>
         <Text style={styles.title}>StudyCube</Text>
-        <Image source={require('./assets/icon.png')} style={{width: '40%', height: '30%', marginTop: 40}} />
+        <Image source={require("./assets/icon.png")} style={styles.logoStyle} />
         <Text style={styles.slogan}>Your little productivity buddy</Text>
-        {this.state.startApp ? <Button className= "enterButton" title="Start Timer" onPress={this.hideButton} />: null}
+        {this.state.startApp ? <Button className="enterButton" title="Start Working" onPress={this.hideButton}  />: null}
         {!this.state.startApp ? <Timer />: null}
-    </View>
-  );
- }
+        {!this.state.startApp ? <Clock />: null}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#30AEEA',
-    alignItems: 'center',
-    fontWeight: 'bold',
+    backgroundColor: "#30AEEA",
+    alignItems: "center",
+    fontWeight: "bold"
   },
   title: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 50,
     marginTop: 30,
-    color: '#BA0ED4',
+    color: "#BA0ED4"
   },
   slogan: {
     marginTop: 30,
     fontSize: 22,
     marginBottom: 20,
-    fontWeight: 'bold',
-    color: '#BA0ED4',
+    fontWeight: "bold",
+    color: "#BA0ED4"
+  },
+  logoStyle: {
+    width: "40%",
+    height: "30%",
+    marginTop: 40
   }
 });
