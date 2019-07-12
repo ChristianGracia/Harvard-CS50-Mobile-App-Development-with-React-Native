a08972aaf230617d7b51257040fd2404c2c6a8fa
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-12 01:31:25 -0400

    ux styling

--- Project-1/components/Timer.js
+++ Project-1/components/Timer.js
@@ -31,49 +31,55 @@ class Timer extends React.Component {
   render() {
     return(
 
-      <View style={styles.container}>
-      <View className="buttons" style={styles.button1}>
-      {this.state.buttonShow ? <Button title="5 Minutes" color="#BA0ED4" onPress={this.hideTimeButtons}/>: null}
-      </View>
-      <View className="buttons"style={styles.button2}>
+      <View>
+             <View style={styles.buttonBoth}>
+                 <View className="buttons" style={styles.button1}>
+                       {this.state.buttonShow ? <Button title="5 Minutes" color="#BA0ED4" onPress={this.hideTimeButtons}/>: null}
+                   </View>
 
-      {this.state.buttonShow ? <Button title= "25 minutes" color="#BA0ED4" onPress={this.hideTimeButtons}/>: null}
-      </View>
+                    <View className="buttons"style={styles.button2}>
+                    {this.state.buttonShow ? <Button title= "25 Minutes" color="#BA0ED4" onPress={this.hideTimeButtons}/>: null}
+                    </View>
+             </View>
+             {this.state.timerShow ? <Text style={styles.timerHeader}>TIMER: </Text>: null}
 
-      {this.state.timerShow ? <Text style={styles.timerHeader}>TIMER</Text>: null}
-      {this.state.timerShow ? <Button title="STOP TIMER" onPress={this.hideTimeButtons}/>: null}
+              <View style={styles.buttonsAlter}>
+              {this.state.timerShow ? <Button title="START TIMER"/>: null}
+              {this.state.timerShow ? <Button title="STOP TIMER"/>: null}
+              {this.state.timerShow ? <Button title="RESET TIMER" onPress={this.hideTimeButtons}/>: null}
+              </View>
+
       </View>
   )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    fontWeight: 'bold',
-    fontSize: 40,
-  },
   button1: {
     borderColor: '#fff',
-    marginRight: 30,
     backgroundColor:'#fff',
-    borderRadius:10,
-    borderWidth: 1,
+    marginBottom: 20,
 
   },
   button2: {
     borderColor: '#fff',
-    marginLeft: 30,
     backgroundColor:'#fff',
-    borderRadius:10,
-    borderWidth: 1,
+    marginTop: 20,
+
+  },
+  buttonBoth: {
+    display: 'flex',
+    flexDirection: 'column',
+    fontWeight: 'bold',
+    fontSize: 40,
   },
   timerHeader: {
-    marginLeft: -50,
     fontWeight: 'bold',
     fontSize: 40,
     color: 'red',
+  },
+  buttonsAlter: {
+
   }
 
 });
