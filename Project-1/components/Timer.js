f2ecffd95e465ea87442094e622ed4c7e75fb378
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-12 17:18:00 -0400

    timer almost complete

--- Project-1/components/Timer.js
+++ Project-1/components/Timer.js
@@ -8,8 +8,6 @@ class Timer extends React.Component {
     super(props);
 
     this.state = {
-      buttonShow: true,
-      timeShow: false,
       min25: false,
       min5: false,
       minutes: "05",
@@ -26,10 +24,10 @@ class Timer extends React.Component {
     this.onButtonStop = this.onButtonStop.bind(this);
     this.onButtonClear = this.onButtonClear.bind(this);
     this.start = this.start.bind(this);
-    // this.startClock = this.startClock.bind(this);
   }
 
     componentDidMount() {
+
         this.start();
     }
 
@@ -45,12 +43,18 @@ class Timer extends React.Component {
                 count = this.state.minutes
 
             if( Number(this.state.seconds) == 0 ) {
+                if (count > 0) {
+
                 count = (Number(this.state.minutes) - 1).toString();
-                num = '59';
-            }
+                  num = '59';}
+                else {
+                  num = '0';
+                  count = '0'
+                }
+              }
 
             self.setState({
-                minutes: count.length == 1 ? '0' + count : count,
+                minutes: count.length == 0 ? '0' + count : count,
                 seconds: num.length == 1 ? '0' + num : num
             });
         }, 1000);
@@ -60,7 +64,7 @@ class Timer extends React.Component {
   onButtonStart = () => {
 
       this.start();
-      this.setState({startClock: true, stopClock: false});
+      this.setState({startClock: true, stopClock: false})
   }
 
 
@@ -73,90 +77,32 @@ class Timer extends React.Component {
   onButtonClear = () => {
       this.setState({
           timer: null,
-          counter: '00',
+          minutes: '00',
           seconds: '00',
       });
   }
 
 
 
-  hideTimeButtons25 = () => {
-    this.setState({ minutes: "00" });
-    this.setState({ seconds: "00" });
-
-    if (this.state.buttonShow == true) {
-      this.setState({ buttonShow: false });
-      this.setState({ timerShow: true });
-      this.setState({ min25: true });
-    } else {
-      this.setState({ buttonShow: true });
-      this.setState({ timerShow: false });
-      this.setState({ min25: false });
-    }
-  };
-
-  hideTimeButtons5 = () => {
-    this.setState({ minutes: "00" });
-    this.setState({ seconds: "00" });
-
-    if (this.state.buttonShow == true) {
-      this.setState({ buttonShow: false });
-      this.setState({ timerShow: true });
-      this.setState({ min5: true });
-    } else {
-      this.setState({ buttonShow: true });
-      this.setState({ timerShow: false });
-      this.setState({ min5: false });
-    }
-  };
-
-//     startClock = () => {
-//
-//     if (this.state.min25 == true)
-//     {
-//       this.setState({ minutes: "25" });
-//       this.setState({ seconds: "00" });
-//     }
-//     else
-//       this.setState({ minutes: "5" });
-//       this.setState({ seconds: "00" });
-// }
-
-
   render() {
     return (
          <View>
-        <Text>{this.state.counter}:{this.state.seconds}</Text>
+         <View style={styles.buttonBoth}>
+
+         <View className="buttons" style={styles.button1}>
+         <Button title="5 Mins" />
+         </View>
+         <View className="buttons" style={styles.button2}>
+         <Button title="25 Mins" />
+         </View>
+         </View>
+
+        <Text>{this.state.minutes}:{this.state.seconds}</Text>
         <Button title="Start" onPress={this.onButtonStart}/>
         <Button title="Stop" onPress={this.onButtonStop}/>
         <Button title="Clear" onPress={this.onButtonClear}/>
-        <View style={styles.buttonBoth}>
-          <View className="buttons" style={styles.button1}>
-            {this.state.buttonShow ? (
-              <Button
-                title="5 Minutes"
-                color="#BA0ED4"
-                onPress={this.hideTimeButtons5}
-              />
-            ) : null}
-          </View>
-
-          <View className="buttons" style={styles.button2}>
-            {this.state.buttonShow ? (
-              <Button
-                title="25 Minutes"
-                color="#BA0ED4"
-                onPress={this.hideTimeButtons25}
-              />
-            ) : null}
-          </View>
-        </View>
 
         <View style={styles.buttonsAlter}>
-
-          {this.state.timerShow ? (
-            <Button title="RESET TIMER" onPress={this.hideTimeButtons5} />
-          ) : null}
         </View>
       </View>
     );
@@ -164,19 +110,21 @@ class Timer extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
   button1: {
     borderColor: "#fff",
     backgroundColor: "#fff",
-    marginBottom: 20
   },
   button2: {
     borderColor: "#fff",
     backgroundColor: "#fff",
-    marginTop: 20
   },
   buttonBoth: {
     display: "flex",
-    flexDirection: "column",
+    flexDirection: "row",
     fontWeight: "bold",
     fontSize: 40
   },
@@ -184,7 +132,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 40,
     color: "red",
-    marginTop: -40
   },
   buttonsAlter: {}
 });
