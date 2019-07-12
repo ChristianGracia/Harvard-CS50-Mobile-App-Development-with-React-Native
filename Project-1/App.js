9197b6ca521202156d7009fc40e8dc2f0ea8def1
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-11 21:57:54 -0400

    updates

--- Project-1/App.js
+++ Project-1/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import Timer from './components/Timer.js'
 
-export default function App() {
+export default class App extends React.Component {
+  state = {
+    showButton: 'visible',
+  };
+
+    hideButton = () => {
+      this.setState({
+        showButton: 'hidden'
+      })
+    };
+
+
+render() {
   return (
     <View style={styles.container}>
-      <Text style={{fontWeight: 'bold', fontSize: 50, marginTop: 60}}>StudyCube</Text>
-      <Image source={require('./assets/icon.png')} style={{width: '40%', height: '30%', marginTop: 80}} />
-      <Text style={{marginTop: 30, fontSize: 24}}>Your little productivity buddy</Text>
+        <Text style={{fontWeight: 'bold', fontSize: 50, marginTop: 30}}>StudyCube</Text>
+        <Image source={require('./assets/icon.png')} style={{width: '40%', height: '30%', marginTop: 40}} />
+        <Text style={{marginTop: 30, fontSize: 22, marginBottom: 20, fontWeight: 'bold'}}>Your little productivity buddy</Text>
+        <Button title="Start Timer" style = {{visibility: this.state.showButton}} onPress = {this.hideButton}/>
+        <Timer />
     </View>
   );
+ }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +32,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#30AEEA',
     alignItems: 'center',
+    fontWeight: 'bold',
   },
 });
---
+++ Project-1/components/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import PropTypes from 'prop-types';
+
+
+class Timer extends React.Component {
+  render() {
+    return(
+    <Button title="hi" />
+  )
+  }
+}
+
+export default Timer;
