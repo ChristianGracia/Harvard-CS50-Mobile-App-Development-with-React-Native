63d4599c5a58512f5c6bd50ce53d67ff43eb436d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-13 16:56:49 -0400

    amazing keyboard work around... homescreen done, second page routed, input text rtained, now for API

--- Project-2/project2/Home.js
+++ Project-2/project2/Home.js
@@ -1,140 +1,155 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
-import AppNavigator from './AppNavigator';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform
+} from "react-native";
+import { StackNavigator, Header } from 'react-navigation';
+import AppNavigator from "./AppNavigator";
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: '',
-      typeCheck: false,
-
-    }
+      query: "",
+      typeCheck: false
+    };
     this.handleSearch = this.handleSearch.bind(this);
   }
 
-    handleSearch = () => {
-      this.props.navigation.navigate('Results')
-      this.setState({typeCheck: false});
-    }
-    changeSearch = (event) => {
-       this.setState({query: event.nativeEvent.text});
-       this.setState({typeCheck: true});
-    }
+  handleSearch = () => {
+    this.props.navigation.navigate("Results");
+    this.setState({ typeCheck: false });
+  };
+  changeSearch = event => {
+    this.setState({ query: event.nativeEvent.text });
+    this.setState({ typeCheck: true });
+  };
 
   render() {
     return (
-      <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <Text style={styles.titleHeader1}><Text style={{...styles.titleHeader1, ...styles.titleHeader2}}>Movie</Text>Wiki</Text>
-          <View style={styles.imageDiv}>
-         <Image style={styles.imageHeader} source={require("./assets/main.png")} />
-         </View>
-
-         <View style={styles.sloganDiv}>
-        <Text style={styles.searchHeader}>Search for any movie!</Text>
+      <View style={styles.container}>
+        <Text style={styles.titleHeader1}>
+          <Text style={{ ...styles.titleHeader1, ...styles.titleHeader2 }}>
+            Movie
+          </Text>
+          Wiki
+        </Text>
+        <View style={styles.imageDiv}>
+          <Image
+            style={styles.imageHeader}
+            source={require("./assets/main.png")}
+          />
+          {this.state.typeCheck ? (
+            <Text style={styles.query}>
+              Find movies related to: {this.state.query}
+            </Text>
+          ) : null}
         </View>
 
-
+        <View style={styles.sloganDiv}>
+          <Text style={styles.searchHeader}>Search for any movie!</Text>
+        </View>
 
         <View style={styles.searchBar}>
-        <View style={styles.submitContainer}>
-        <TextInput
-        style={styles.searchText}
-        placeholder = "Search Here!"
-        placeholderTextColor = "#000"
-        onChange = {this.changeSearch}
-        value={this.state.query}
-        />
-        <TouchableOpacity style={styles.submit} onPress={this.handleSearch}><Text style={styles.submitText}>Submit</Text></TouchableOpacity>
-        </View>
-        </View>
-        {this.state.typeCheck ? <Text style={styles.query}>Find movies related to: {this.state.query}</Text>: null}
-        <Text style={styles.bottomGreen}>h</Text>
-      </KeyboardAvoidingView>
+          <View style={styles.submitContainer}>
+            <TextInput
+              style={styles.searchText}
+              placeholder="Search Here!"
+              placeholderTextColor="#000"
+              onChange={this.changeSearch}
+              value={this.state.query}
+            />
+            <TouchableOpacity style={[styles.submit, { paddingBottom: this.keyboardHeight }]} onPress={this.handleSearch}>
+              <Text style={styles.submitText}>Submit</Text>
+            </TouchableOpacity>
 
+        </View>
+          </View>
+
+        <Text style={styles.bottomGreen}>h</Text>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     alignItems: "center",
-    fontWeight: "bold",
+    fontWeight: "bold"
   },
   searchHeader: {
     fontSize: 27,
-    fontWeight: 'bold',
-        color: '#26EA20'
+    fontWeight: "bold",
+    color: "#26EA20"
   },
   titleHeader1: {
     fontSize: 40,
-    fontWeight: 'bold',
-    backgroundColor: '#26EA20',
-    color: '#000000',
+    fontWeight: "bold",
+    backgroundColor: "#26EA20",
+    color: "#000000",
     width: 375,
     height: 80,
-    textAlign: 'center',
-    paddingTop: 7,
-
+    textAlign: "center",
+    paddingTop: 7
   },
-  titleHeader2:{
-    color: '#FFFFFF',
+  titleHeader2: {
+    color: "#FFFFFF"
   },
-  searchBar: {
-
-  }
-  ,
+  searchBar: {},
   searchText: {
-      height: 40,
-      borderColor: '#25EA20',
-      borderWidth: 1,
-      width: 190,
-      paddingLeft: 10,
-      fontWeight: 'bold',
+    height: 40,
+    borderColor: "#25EA20",
+    borderWidth: 1,
+    width: 190,
+    paddingLeft: 10,
+    fontWeight: "bold",
   },
   submit: {
     backgroundColor: "#26EA20",
-    alignItems: 'center',
+    alignItems: "center",
     height: 40,
     width: 110,
-    justifyContent: 'center',
+    justifyContent: "center",
     marginLeft: 40
-  }
-  ,
-  submitText:{
-    fontWeight: 'bold',
-    textAlign: 'center',
+  },
+  submitText: {
+    fontWeight: "bold",
+    textAlign: "center",
     color: "#fff"
   },
   imageDiv: {
-  paddingTop: 80,
-  alignItems: 'center',
+    paddingTop: 80,
+    alignItems: "center"
   },
   sloganDiv: {
     paddingTop: 60,
-    paddingBottom: 20,
-
+    paddingBottom: 20
   },
   submitContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
     paddingTop: 40,
     paddingBottom: 20
   },
   bottomGreen: {
     color: "#26EA20",
-    backgroundColor: '#26EA20',
+    backgroundColor: "#26EA20",
     width: 375,
     height: 30,
     paddingTop: 100,
     marginTop: 90
   },
   query: {
-    fontWeight:'bold',
+    fontWeight: "bold",
     marginTop: 20
-
   }
 });
