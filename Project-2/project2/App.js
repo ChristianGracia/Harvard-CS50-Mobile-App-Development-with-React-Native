c7cbc4f7e13138316304887dc2ecf1151982e226
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-13 03:38:39 -0400

    finally got ide working again with expo and android studio, setting up app now

--- Project-2/project2/App.js
+++ Project-2/project2/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
   }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleHeader}>MovieSearch</Text>
+       <Image source={require("./assets/icon.png")} />
+      <Text style={styles.searchHeader}>Search for any movie!</Text>
+      <TextInput   style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+             onChangeText={(text) => this.setState({text})}
+             value={this.state.text}/>
+
+     />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +29,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  searchHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  titleHeader: {
+    fontSize: 50,
+    fontWeight: 'bold'
+  }
 });
