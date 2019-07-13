c339d9dfd6d1ab1065745085750c2776799704f5
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-13 04:22:53 -0400

    main search page styling

--- Project-2/project2/App.js
+++ Project-2/project2/App.js
@@ -1,33 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
+
+
     this.state = {
+      text: "",
     }
   }
-  return (
-    <View style={styles.container}>
-      <Text style={styles.titleHeader}>MovieSearch</Text>
-       <Image source={require("./assets/icon.png")} />
-      <Text style={styles.searchHeader}>Search for any movie!</Text>
-      <TextInput   style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-             onChangeText={(text) => this.setState({text})}
-             value={this.state.text}/>
 
-     />
+  render() {
+    return(
+    <View style={styles.container}>
+      <Text style={styles.titleHeader}>MovieWiKi</Text>
+       <Image source={require("./assets/main.png")} />
+      <Text style={styles.searchHeader}>Search for any movie!</Text>
+
+
+      <View style={styles.SearchBar}>
+      <TextInput
+      style={styles.searchText}
+      placeholder = "Search Here!"
+      placeholderTextColor = "#000000"
+      onChangeText={(text) => this.setState({text})}
+      value={this.state.text}
+      />
+      <TouchableOpacity><Text>Search</Text></TouchableOpacity>
+      </View>
+
+
     </View>
   );
-}
+}}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    fontWeight: "bold"
   },
   searchHeader: {
     fontSize: 20,
@@ -36,5 +49,15 @@ const styles = StyleSheet.create({
   titleHeader: {
     fontSize: 50,
     fontWeight: 'bold'
+  },
+  searchBar: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  searchText: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      width: 160
   }
 });
