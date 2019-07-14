8e27089970afaa94486e754a4dab5f9efcf40a87
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-14 02:28:48 -0400

    removed warning ignore I used during development

--- Project-2/project2/App.js
+++ Project-2/project2/App.js
@@ -4,11 +4,6 @@ import AppNavigator from './AppNavigator';
 import { Home } from "./Home"
 import { Results } from "./Results"
 
-//remove require cycle unnecessary warning
-console.disableYellowBox = true;
-
-
-
 export default class App extends React.Component {
   render() {
     return (
